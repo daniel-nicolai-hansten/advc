bdd049d2435e001b27e47910a3739a66dc9c6396
Ubuntu <dnh@dnh-ubnt.cybv2n1f000efewkkm4epllkab.oslx.internal.cloudapp.net>
2022-12-21 12:08:17 +0000

    Funker

--- adv-c21/src/main.rs
+++ adv-c21/src/main.rs
@@ -1,72 +1,128 @@
-use nom::branch::alt;
 use nom::bytes::complete::take_until;
-use nom::character::complete::alpha1;
 use nom::character::complete::digit1;
 use nom::sequence::preceded;
 use nom::{
-    bytes::complete::tag,
-    character::complete,
-    error::context,
-    sequence::{delimited, separated_pair},
+    bytes::complete::{tag, take},
     *,
 };
+use std::collections::HashMap;
+use std::fs;
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("./input.txt").unwrap();
+    let (mut high, mut low) = (false, false);
+    let mut lastnum = 0;
+    'outer: for i in 3093175982000..3093175983000 {
+        let number = i;
+        let (monkeylist, mut monkeys) = parse_input(&input);
+        let human = Monkey {
+            name: usize::from_str_radix("humn", 36).unwrap(),
+            num: MonkeyNumber::Num(number),
+        };
+        monkeys.insert(human.name, human);
+        'inner: loop {
+            for mnky in &monkeylist {
+                let monkey = monkeys.get(&mnky).unwrap();
+                let mut res = None;
+                match &monkey.num {
+                    MonkeyNumber::Num(_) => (),
+                    MonkeyNumber::MathOperation((monkey1, monkey2, op)) => {
+                        if let MonkeyNumber::Num(mnky1_num) = monkeys.get(&monkey1).unwrap().num {
+                            if let MonkeyNumber::Num(mnky2_num) = monkeys.get(&monkey2).unwrap().num
+                            {
+                                match op {
+                                    Operation::Divide => res = Some(mnky1_num / mnky2_num),
+                                    Operation::Multiply => res = Some(mnky1_num * mnky2_num),
+                                    Operation::Add => res = Some(mnky1_num + mnky2_num),
+                                    Operation::Sub => res = Some(mnky1_num - mnky2_num),
+                                }
+                            }
+                        }
+                    }
+                }
+                if let Some(num) = res {
+                    if mnky == &usize::from_str_radix("lttc", 36).unwrap() {
+                        if num < 89661494901968 {
+                            print!(".");
+                            high = true;
+                        } else if num > 89661494901968 {
+                            print!("+");
+                            lastnum = number;
+                            low = true;
+                        } else if num == 89661494901968 {
+                            println!("2:{} i: {}", num, i);
+                            break 'outer;
+                        }
+                    }
+                    if high && low {
+                        println!("number just under {} and over {}", number, lastnum);
+                        break 'outer;
+                    }
+                    let mut newmonkey = monkey.clone();
+                    newmonkey.num = MonkeyNumber::Num(num);
+                    monkeys.insert(newmonkey.name, newmonkey);
+                    if mnky == &usize::from_str_radix("root", 36).unwrap() {
+                        break 'inner;
+                    }
+                }
+            }
+        }
+    }
+    //let result = monkeys.get(&usize::from_str_radix("root", 36).unwrap());
+    //println!("{:?}", result);
 }
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 struct Monkey {
     name: usize,
     num: MonkeyNumber,
 }
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 enum MonkeyNumber {
     Num(i64),
     MathOperation((usize, usize, Operation)),
 }
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 enum Operation {
     Divide,
     Multiply,
     Add,
     Sub,
 }
-fn parse_input(input: &str) -> Vec<Monkey> {
-    let mut monkeys = Vec::new();
-    for line in input.lines() {}
-    monkeys
+fn parse_input(input: &str) -> (Vec<usize>, HashMap<usize, Monkey>) {
+    let mut monkeylist = HashMap::new();
+    let mut monkeylist2 = Vec::new();
+    for line in input.lines() {
+        if let Ok((_, mnky)) = parse_monkey(line) {
+            //println!("{:?}", mnky);
+            monkeylist2.push(mnky.name);
+            monkeylist.insert(mnky.name, mnky);
+        }
+    }
+    (monkeylist2, monkeylist)
 }
 
 fn parse_monkey(input: &str) -> IResult<&str, Monkey> {
     let (input, id) = take_until(": ")(input)?;
-    let (_, num): (&str,  MonkeyNumber) = alt( (preceded(tag(": "), digit1), preceded(tag(": "), alpha1) )).map(|num: &str| {
-        if num.len() == 11 {
-            MonkeyNumber::Num(i64::from_str_radix("2", 10).unwrap())
-        } else {
-            MonkeyNumber::Num(i64::from_str_radix(num, 10).unwrap())
-        }
-    })(input)?;
-    // let (input, clay) =
-    //     delimited(tag(" Each clay robot costs "), complete::u64, tag(" ore."))(input)?;
-    // let (input, obsidian) = delimited(
-    //     tag(" Each obsidian robot costs "),
-    //     separated_pair(complete::u64, tag(" ore and "), complete::u64).map(|pair| {
-    //         ObsidianRequirements {
-    //             ore: pair.0 as usize,
-    //             clay: pair.1 as usize,
-    //         }
-    //     }),
-    //     tag(" clay."),
-    // )(input)?;
-    // let (input, geode) = delimited(
-    //     tag(" Each geode robot costs "),
-    //     separated_pair(complete::u64, tag(" ore and "), complete::u64).map(|pair| {
-    //         GeodeRequirements {
-    //             ore: pair.0 as usize,
-    //             obsidian: pair.1 as usize,
-    //         }
-    //     }),
-    //     tag(" obsidian."),
-    // )(input)?;
+    let mut num;
+    if input.len() == 13 {
+        let (input, monkey1) = preceded(tag(": "), take(4 as usize))(input)?;
+        let (input, operationchar) = preceded(tag(" "), take(1 as usize))(input)?;
+        let (_input, monkey2) = preceded(tag(" "), take(4 as usize))(input)?;
+        num = MonkeyNumber::MathOperation((
+            usize::from_str_radix(monkey1, 36).unwrap(),
+            usize::from_str_radix(monkey2, 36).unwrap(),
+            match operationchar {
+                "/" => Operation::Divide,
+                "+" => Operation::Add,
+                "-" => Operation::Sub,
+                "*" => Operation::Multiply,
+                _ => Operation::Multiply,
+            },
+        ));
+    } else {
+        let (_input, number) = preceded(tag(": "), digit1)(input)?;
+        num = MonkeyNumber::Num(i64::from_str_radix(number, 10).unwrap());
+    }
+
     Ok((
         input,
         Monkey {
@@ -100,6 +156,7 @@ mod tests {
     fn it_works() {
         let (_, result) = parse_monkey("dbpl: 5").unwrap();
         let (_, result2) = parse_monkey("pppw: cczh / lfqf").unwrap();
+        println!("{:?}", result2);
         assert_eq!(
             Monkey {
                 name: 621705,
