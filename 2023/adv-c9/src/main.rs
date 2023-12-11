0210b8acfed4547d32ecf588dba853fcd5433aae
daniel-nicolai-hansten <74103470+daniel-nicolai-hansten@users.noreply.github.com>
2023-12-11 12:48:14 +0100

    fixes

--- 2023/adv-c9/src/main.rs
+++ 2023/adv-c9/src/main.rs
@@ -10,20 +10,20 @@ fn main() {
         .collect();
     let p1 = oasis_historys
         .iter()
-        .fold(0, |acc, v| acc + predict(v, Dir::Fwd));
+        .fold(0, |acc, history| acc + predict(history, Dir::Fwd));
     let p2 = oasis_historys
         .iter()
-        .fold(0, |acc, v| acc + predict(v, Dir::Rev));
+        .fold(0, |acc, history| acc + predict(history, Dir::Rev));
     println!("Pt1: {p1} Pt2: {p2}");
 }
 
 fn predict(values: &[i32], dir: Dir) -> i32 {
     let diffs: Vec<i32> = values.windows(2).map(|v| v[1] - v[0]).collect();
     let allzeros = !values.iter().fold(false, |acc, x| acc || *x != 0);
-    match (allzeros, dir) {
+    match (allzeros, dir) {¨
+        (true, _) => 0,
         (false, Dir::Fwd) => values.last().unwrap() + predict(&diffs, dir),
         (false, Dir::Rev) => values.first().unwrap() - predict(&diffs, dir),
-        (true, _) => 0,
     }
 }
 
