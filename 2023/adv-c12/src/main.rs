62b3044234c3c5a7aad2bb08189c7147e1956166
Daniel N. Hansten <dnh2000@gmail.com>
2023-12-12 10:40:54 +0100

    dp

--- 2023/adv-c12/src/main.rs
+++ 2023/adv-c12/src/main.rs
@@ -6,9 +6,11 @@ fn main() {
     let (tx, rx) = mpsc::channel();
     springs.par_iter().for_each(|(spring, nums)| {
         let tx = tx.clone();
-        let possible_springs = generate_variance(&spring, &nums);
+        // let pairs: Vec<&[Spring]> = spring.split(|s| s == &Spring::Operational).collect();
+
+        let possible_springs = count_variants(&spring, nums);
         let sum = possible_springs;
-        println!(".");
+        // println!(".");
         let _ = tx.send(sum);
     });
     drop(tx);
@@ -18,7 +20,7 @@ fn main() {
     }
     println!("sum: {totsum}");
 }
-fn parse_input(input: &str) -> Vec<(Vec<Spring>, Vec<u32>)> {
+fn parse_input(input: &str) -> Vec<(Vec<Spring>, Vec<usize>)> {
     let mut ret = vec![];
     let gearsplit = |c| match c {
         '#' => Spring::Broken,
@@ -29,12 +31,12 @@ fn parse_input(input: &str) -> Vec<(Vec<Spring>, Vec<u32>)> {
     for line in input.lines() {
         let splits: Vec<&str> = line.trim_start().split_ascii_whitespace().collect();
         let gearstates: Vec<Spring> = splits[0].chars().map(gearsplit).collect();
-        let nums: Vec<u32> = splits[1]
+        let nums: Vec<usize> = splits[1]
             .split(",")
-            .map(|x| x.parse::<u32>().unwrap())
+            .map(|x| x.parse::<usize>().unwrap())
             .collect();
         let mut gearstates_p2: Vec<Spring> = vec![];
-        let mut nums_p2: Vec<u32> = vec![];
+        let mut nums_p2: Vec<usize> = vec![];
         for i in 0..5 {
             let mut new_gearstates = gearstates.clone();
             if i < 4 {
@@ -47,58 +49,37 @@ fn parse_input(input: &str) -> Vec<(Vec<Spring>, Vec<u32>)> {
     }
     ret
 }
-fn cmp_springs(springlist: &[Spring], nums: &[u32]) -> bool {
-    let mut last = Spring::Unknown;
-    let mut brokens = 0;
-    let mut springs = vec![];
-    for spring in springlist {
-        use Spring as S;
-        match (spring, last) {
-            (S::Broken, _) => brokens += 1,
-            (S::Operational, S::Broken) => {
-                springs.push(brokens);
-                brokens = 0;
+
+fn count_variants(springs: &[Spring], counts: &[usize]) -> usize {
+    let n = springs.len();
+    let m = counts.len();
+    let mut dp = vec![vec![vec![0; n + 1]; m + 1]; n + 1];
+    dp[n][m][0] = 1;
+    dp[n][m - 1][counts[m - 1]] = 1;
+    use Spring as S;
+    for pos in (0..n).rev() {
+        for (group, &max_count) in counts.iter().enumerate() {
+            for count in 0..=max_count {
+                for c in [S::Operational, S::Broken] {
+                    if springs[pos] == c || springs[pos] == S::Unknown {
+                        if c == S::Operational && count == 0 {
+                            dp[pos][group][count] += dp[pos + 1][group][0];
+                        } else if c == S::Operational && group < m && counts[group] == count {
+                            dp[pos][group][count] += dp[pos + 1][group + 1][0];
+                        } else if c == S::Broken {
+                            dp[pos][group][count] += dp[pos + 1][group][count + 1];
+                        }
+                    }
+                }
             }
-            _ => (),
         }
-        last = *spring;
-    }
-    if brokens != 0 {
-        springs.push(brokens);
-    }
-    // println!("nums {nums:?}, springs {springs:?}");
-    nums == springs
-}
-
-fn generate_variance(springs: &[Spring], nums: &[u32]) -> usize {
-    let unknowns = springs.iter().filter(|s| **s == Spring::Unknown).count();
-    let two: u32 = 2;
-    let combinations = two.pow(unknowns as u32);
-    let mut ret = 0;
-
-    for i in 0..combinations {
-        let mut unknows_ret = 0;
-        let replace_unknown = |s: &Spring| {
-            let mask = 1 << unknows_ret;
-            match (s, mask & i != 0) {
-                (Spring::Unknown, true) => {
-                    unknows_ret += 1;
-                    Spring::Broken
-                }
-                (Spring::Unknown, false) => {
-                    unknows_ret += 1;
-                    Spring::Operational
-                }
-                _ => *s,
-            }
-        };
-        let fixed_springs: Vec<Spring> = springs.iter().map(replace_unknown).collect();
-        if cmp_springs(&fixed_springs, nums) {
-            ret += 1;
+        if matches!(springs[pos], Spring::Operational | Spring::Unknown) {
+            dp[pos][m][0] += dp[pos + 1][m][0];
         }
     }
-    ret
+    dp[0][0][0]
 }
+
 const TESTINPUT: &str = "???.### 1,1,3
 .??..??...?##. 1,1,3";
 #[cfg(test)]
@@ -112,21 +93,21 @@ mod tests {
         let (spring, num) = &springs[0];
         assert!(cmp_springs(spring, num));
     }
-    #[test]
-    fn it_works2() {
-        let springs = parse_input(TESTINPUT);
-        let mut sum = 0;
-        for (spring, nums) in &springs {
-            let possible_springs = generate_variance(&spring, &nums);
-            for spring in possible_springs {
-                if cmp_springs(&spring, nums) {
-                    println!("spring: {spring:?}");
-                    sum += 1;
-                }
-            }
-        }
-        println!("sum: {sum}");
-    }
+    // #[test]
+    // fn it_works2() {
+    //     let springs = parse_input(TESTINPUT);
+    //     let mut sum = 0;
+    //     for (spring, nums) in &springs {
+    //         let possible_springs = generate_variance(&spring, &nums);
+    //         for spring in possible_springs {
+    //             if cmp_springs(&spring, nums) {
+    //                 println!("spring: {spring:?}");
+    //                 sum += 1;
+    //             }
+    //         }
+    //     }
+    //     println!("sum: {sum}");
+    // }
 }
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Spring {
