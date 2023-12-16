118f16488ad49472f2198c9828b5d94b5ce2b44d
daniel-nicolai-hansten <daniel.hansten@bouvet.no>
2023-12-16 10:10:12 +0100

    commis

--- aoc2023/src/day16.rs
+++ aoc2023/src/day16.rs
@@ -1,7 +1,7 @@
-use std::collections::{HashSet, VecDeque};
+use aoc_runner_derive::{aoc, aoc_generator};
 use itertools::Itertools;
 use rayon::prelude::*;
-use aoc_runner_derive::{aoc, aoc_generator};
+use std::collections::{HashSet, VecDeque};
 #[aoc_generator(day16)]
 fn parse(input: &str) -> Vec<Vec<char>> {
     let mut ret = vec![];
@@ -55,9 +55,9 @@ impl Pos {
 
 #[aoc(day16, part1)]
 fn part1(input: &[Vec<char>]) -> usize {
-    let startpos = Pos {x: 0, y: 0};
+    let startpos = Pos { x: 0, y: 0 };
     let startdir = Dir::Right;
-beam_energy(input, startpos, startdir)
+    beam_energy(input, startpos, startdir)
 }
 fn beam_energy(input: &[Vec<char>], startpos: Pos, startdir: Dir) -> usize {
     let max_x = input[0].len();
@@ -131,14 +131,24 @@ fn beam_energy(input: &[Vec<char>], startpos: Pos, startdir: Dir) -> usize {
         }
     }
 
-    visited.iter().map(|(p, _d)|p).sorted().dedup().fold(0, |acc, _p| acc +1 )
+    visited
+        .iter()
+        .map(|(p, _d)| p)
+        .sorted()
+        .dedup()
+        .fold(0, |acc, _p| acc + 1)
 }
 
 #[allow(dead_code)]
 fn map_printer(map: &[Vec<char>], visited: &HashSet<(Pos, Dir)>) {
     for (y, line) in map.iter().enumerate() {
-        for (x, c ) in line.iter().enumerate()  {
-           let sym =  match (visited.contains(&(Pos{x,y}, Dir::Down)), visited.contains(&(Pos{x,y}, Dir::Up)), visited.contains(&(Pos{x,y}, Dir::Left)), visited.contains(&(Pos{x,y}, Dir::Right))) {
+        for (x, c) in line.iter().enumerate() {
+            let sym = match (
+                visited.contains(&(Pos { x, y }, Dir::Down)),
+                visited.contains(&(Pos { x, y }, Dir::Up)),
+                visited.contains(&(Pos { x, y }, Dir::Left)),
+                visited.contains(&(Pos { x, y }, Dir::Right)),
+            ) {
                 (true, _, _, _) => 'v',
                 (_, true, _, _) => '^',
                 (_, _, true, _) => '<',
@@ -146,7 +156,6 @@ fn map_printer(map: &[Vec<char>], visited: &HashSet<(Pos, Dir)>) {
                 _ => *c,
             };
             print!("{sym}");
-
         }
         println!();
     }
@@ -157,22 +166,23 @@ fn part2(input: &[Vec<char>]) -> usize {
     let max_y = input.len();
     let max_x = input[0].len();
     for x in 0..max_x {
-        wq.push((Pos {y:0, x}, Dir::Down));
-        wq.push((Pos {y:max_y -1, x}, Dir::Up));
+        wq.push((Pos { y: 0, x }, Dir::Down));
+        wq.push((Pos { y: max_y - 1, x }, Dir::Up));
     }
     for y in 0..max_y {
-        wq.push((Pos {y, x:0}, Dir::Right));
-        wq.push((Pos {y, x: max_x -1}, Dir::Left));
+        wq.push((Pos { y, x: 0 }, Dir::Right));
+        wq.push((Pos { y, x: max_x - 1 }, Dir::Left));
     }
-    let res = wq.par_iter().map(|(p, d)| beam_energy(input, *p, *d)).max()
-   res
+    wq.par_iter()
+        .map(|(p, d)| beam_energy(input, *p, *d))
+        .max()
+        .unwrap()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    const TESTINPUT: &str = 
-r".|...\....
+    const TESTINPUT: &str = r".|...\....
 |.-.\.....
 .....|-...
 ........|.
