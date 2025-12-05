616c008bc925fed2c7aa93d0cbd8f6d4e9c6b664
Daniel N. Hansten <dnh@equinor.com>
2025-12-05 09:22:13 +0100

    improve prefromance

--- aoc2025/src/day3.rs
+++ aoc2025/src/day3.rs
@@ -1,41 +1,31 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 #[aoc_generator(day3)]
-fn parse(input: &str) -> Vec<Vec<u8>> {
+fn parse(input: &str) -> Vec<Vec<u64>> {
     input
         .lines()
-        .map(|line| line.chars().map(|c| c.to_digit(10).unwrap() as u8).collect())
+        .map(|line| line.chars().map(|c| c.to_digit(10).unwrap() as u64).collect())
         .collect()
 }
 
 #[aoc(day3, part1)]
-fn part1(input: &[Vec<u8>]) -> u64 {
-    let mut sum = 0;
-    for banks in input {
-        let volt = banks[..banks.len() - 1].iter().max().unwrap();
-        let pos = banks.iter().position(|&v| v == *volt).unwrap();
-        let volt2 = banks[pos + 1..].iter().max_by_key(|&v| v).unwrap();
-        let volt = volt * 10 + volt2;
-        sum += volt as u64;
+fn part1(input: &[Vec<u64>]) -> u64 {
+    input.iter().map(|banks| findnum(banks, 2)).sum()
+}
+
+fn findnum(banks: &[u64], digits: usize) -> u64 {
+    let (mut num, mut pos) = (0, 0);
+    for digit in (0..digits).rev() {
+        let bankslice = &banks[pos..banks.len() - digit];
+        let volt = bankslice.iter().max().unwrap();
+        pos = bankslice.iter().position(|&v| v == *volt).unwrap() + 1 + pos;
+        num += *volt * 10u64.pow(digit as u32);
     }
-    sum
+    num
 }
 
 #[aoc(day3, part2)]
-fn part2(input: &[Vec<u8>]) -> u64 {
-    let mut sum = 0;
-
-    for banks in input {
-        let mut numarr = vec![];
-        let mut pos = 0;
-        for digit in (0..12).rev() {
-            let bankslice = &banks[pos..banks.len() - digit];
-            let volt = bankslice.iter().max().unwrap();
-            pos = bankslice.iter().position(|&v| v == *volt).unwrap() + 1 + pos;
-            numarr.push(*volt);
-        }
-        sum += numarr.iter().rev().enumerate().map(|(i, &d)| (d as u64) * 10u64.pow(i as u32)).sum::<u64>();
-    }
-    sum
+fn part2(input: &[Vec<u64>]) -> u64 {
+    input.iter().fold(0, |acc, banks| findnum(banks, 12) + acc)
 }
 
 #[cfg(test)]
--- aoc2025/src/day4.rs
+++ aoc2025/src/day4.rs
@@ -1,16 +1,14 @@
+use std::collections::VecDeque;
+
 use aoc_runner_derive::{aoc, aoc_generator};
-use common::pos::{Direction, Pos, Position, PositionError};
+use common::pos::Pos;
 #[aoc_generator(day4)]
 fn parse(input: &str) -> Vec<Vec<bool>> {
     let mut map = Vec::new();
     for line in input.lines() {
         let mut row = Vec::new();
         for c in line.chars() {
-            row.push(match c {
-                '@' => true,
-                '.' => false,
-                c => panic!("Invalid character in input: {}", c),
-            });
+            row.push(c == '@');
         }
         map.push(row);
     }
@@ -19,14 +17,11 @@ fn parse(input: &str) -> Vec<Vec<bool>> {
 
 #[aoc(day4, part1)]
 fn part1(input: &Vec<Vec<bool>>) -> u64 {
-    let rows = input.len();
-    let cols = input[0].len();
     let mut count = 0;
     for (y, row) in input.iter().enumerate() {
         for (x, &cell) in row.iter().enumerate() {
             if cell {
                 let mut neighbors = 0;
-                // Check neighbors
                 let pos = (y, x);
                 for neighbor in pos.all_neighbor_positions() {
                     let (ny, nx) = neighbor;
@@ -47,36 +42,28 @@ fn part1(input: &Vec<Vec<bool>>) -> u64 {
 
 #[aoc(day4, part2)]
 fn part2(input: &Vec<Vec<bool>>) -> u64 {
-    let rows = input.len();
-    let cols = input[0].len();
     let mut count = 0;
-    let mut pos_removed = vec![];
-    loop {
-        let mut any_removed = false;
-        for (y, row) in input.iter().enumerate() {
-            for (x, &cell) in row.iter().enumerate() {
-                if cell && !pos_removed.contains(&(y, x)) {
-                    let mut neighbors = 0;
-                    // Check neighbors
-                    let pos = (y, x);
-                    for neighbor in pos.all_neighbor_positions() {
-                        let (ny, nx) = neighbor;
-                        if let Some(true) = input.get(ny).and_then(|r| r.get(nx)) {
-                            if input[ny][nx] && !pos_removed.contains(&neighbor) {
-                                neighbors += 1;
-                            }
-                        }
-                    }
-                    if neighbors < 4 {
-                        count += 1;
-                        pos_removed.push(pos);
-                        any_removed = true;
-                    }
-                }
-            }
+    let mut input = input.clone();
+    // seed work queue with all '@' positions
+    let mut wq: Vec<(usize, usize)> = input
+        .iter()
+        .enumerate()
+        .flat_map(|(y, row)| row.iter().enumerate().filter_map(move |(x, &cell)| cell.then_some((x, y))))
+        .collect();
+    while let Some(pos) = wq.pop() {
+        if !input[pos.1][pos.0] {
+            continue;
         }
-        if !any_removed {
-            break;
+        // filter for neighbors within bounds that are still '@'
+        let neighbors = pos
+            .all_neighbor_positions()
+            .iter()
+            .filter_map(|&(nx, ny)| input.get(ny).and_then(|r| r.get(nx).and_then(|p| p.then_some((nx, ny)))))
+            .collect::<Vec<_>>();
+        if neighbors.len() < 4 {
+            count += 1;
+            input[pos.1][pos.0] = false;
+            wq.extend(neighbors);
         }
     }
     count
@@ -102,6 +89,6 @@ mod tests {
 
     #[test]
     fn part2_example() {
-        assert_eq!(part2(&parse(TESTINPUT)), 13);
+        assert_eq!(part2(&parse(TESTINPUT)), 43);
     }
 }
