c6286cc3c1647d72d0d9b1d319bb966ce7734d97
Daniel N. Hansten <dnh2000@gmail.com>
2025-12-10 08:03:28 +0100

    d10p2 old
    Add good_lp solver

--- aoc2025/src/day10.rs
+++ aoc2025/src/day10.rs
@@ -3,12 +3,14 @@ use nom::Parser;
 use nom::character::complete::{char, one_of, space0};
 use nom::multi::{many1, separated_list1};
 use nom::{IResult, bytes::complete::tag, sequence::delimited};
+
+use std::collections::{HashSet, VecDeque};
 use std::vec;
 #[aoc_generator(day10)]
 fn parse(input: &str) -> Vec<(Vec<bool>, Vec<Vec<u64>>, Vec<u64>)> {
     let mut ret = vec![];
     for line in input.lines() {
-        let (rest, v) = lineparse(line).unwrap();
+        let (_rest, v) = lineparse(line).unwrap();
         ret.push(v);
     }
     ret
@@ -22,22 +24,58 @@ fn lineparse(input: &str) -> IResult<&str, (Vec<bool>, Vec<Vec<u64>>, Vec<u64>)>
     Ok((input, (bools, switches, jolts)))
 }
 
-fn parse_bool_array(input: &str) -> nom::IResult<&str, Vec<bool>> {
-    let (input, chars) = delimited(tag("["), many1(one_of(".#")), tag("]")).parse(input)?;
-
-    let bools = chars.into_iter().map(|c| c == '#').collect();
-
-    Ok((input, bools))
-}
-
 #[aoc(day10, part1)]
 fn part1(input: &[(Vec<bool>, Vec<Vec<u64>>, Vec<u64>)]) -> u64 {
-    todo!()
+    let mut total = 0;
+    for (lights, switches, _jolts) in input {
+        let current_light = vec![false; lights.len()];
+        let mut visited: HashSet<Vec<bool>> = HashSet::default();
+        let mut wq = VecDeque::new();
+        wq.push_back((current_light.clone(), 0));
+        while let Some((state, step)) = wq.pop_front() {
+            if visited.contains(&state) {
+                continue;
+            }
+            visited.insert(state.clone());
+            if state == *lights {
+                total += step;
+                break;
+            }
+            for switch in switches {
+                let mut new_state = state.clone();
+                for &idx in switch {
+                    let idx = idx as usize;
+                    new_state[idx] = !new_state[idx];
+                }
+                wq.push_back((new_state, step + 1));
+            }
+        }
+    }
+    total
 }
 
 #[aoc(day10, part2)]
 fn part2(input: &[(Vec<bool>, Vec<Vec<u64>>, Vec<u64>)]) -> u64 {
-    todo!()
+    use good_lp::*;
+    let mut sum = 0;
+    for (_lights, switches, jolts) in input {
+        let mut vars = variables!();
+        let press_vars = (0..switches.len()).map(|_| vars.add(variable().min(0).integer())).collect::<Vec<_>>();
+
+        let mut problem = highs(vars.minimise(press_vars.iter().sum::<Expression>()));
+        let mut exprs = vec![0.into_expression(); jolts.len()];
+        for (i, x) in switches.iter().enumerate() {
+            for &x in x {
+                exprs[x as usize] += press_vars[i];
+            }
+        }
+        for (e, &j) in exprs.into_iter().zip(jolts) {
+            problem.add_constraint(e.eq(j as f64));
+        }
+        let sol = problem.solve().unwrap();
+        sum += press_vars.iter().map(|&v| sol.value(v)).sum::<f64>() as u64;
+    }
+    sum
 }
 
 #[cfg(test)]
@@ -49,13 +87,11 @@ mod tests {
 ";
     #[test]
     fn part1_example() {
-        let parsed = parse(TESTINPUT);
-        println!("{:?}", parsed);
-        // assert_eq!(part1(&parse(TESTINPUT)), 50);
+        assert_eq!(part1(&parse(TESTINPUT)), 7);
     }
 
     #[test]
     fn part2_example() {
-        // assert_eq!(part2(&parse(TESTINPUT)), 50);
+        assert_eq!(part2(&parse(TESTINPUT)), 33);
     }
 }
